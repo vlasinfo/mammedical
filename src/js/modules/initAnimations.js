42b48a4cc412b16b5581c22bd422b81a9102c5bf
Vlasenko Yurii <vlasenkoj@gmail.com>
2026-01-25 13:04:51 +0200

    start about section 2

--- src/js/modules/initAnimations.js
+++ src/js/modules/initAnimations.js
@@ -1,44 +1,49 @@
 export default function initAnimations(scope = document) {
+  return gsap.context(() => {
 
-  const ctx = gsap.context(() => {
-
-    const title = scope.querySelector('.vi-animate-title');
-    if (!title) return;
-
-    const split = new SplitText(title, {
-      type: 'chars'
-    });
-
-    gsap.from(split.chars, {
-      yPercent: 100,
-      opacity: 0,
-      duration: 1,
-      ease: 'power3.out',
-      stagger: 0.05,
-      delay: 0.4,
-      clearProps: 'all'
-    });
-
-        /* ======================
-      DESCRIPTION
+    /* ======================
+      TITLES
     ====================== */
-    const descs = scope.querySelectorAll('.vi-animate-desc');
+    const titles = scope.querySelectorAll(".vi-animate-title");
+
+    titles.forEach(title => {
+      const split = new SplitText(title, { type: "chars" });
+
+      gsap.from(split.chars, {
+        yPercent: 100,
+        opacity: 0,
+        duration: 1,
+        ease: "power3.out",
+        stagger: 0.05,
+        clearProps: "all",
+        scrollTrigger: {
+          trigger: title,
+          start: "top 80%",
+          once: true
+        },
+        onComplete: () => split.revert()
+      });
+    });
+
+    /* ======================
+      DESCRIPTIONS
+    ====================== */
+    const descs = scope.querySelectorAll(".vi-animate-desc");
 
     gsap.from(descs, {
       y: 20,
       opacity: 0,
-      filter: 'blur(6px)',
+      filter: "blur(6px)",
       duration: 0.8,
-      ease: 'power2.out',
-      delay: 1,
+      ease: "power2.out",
       stagger: 0.15,
-      clearProps: 'all'
+      clearProps: "all",
+      scrollTrigger: {
+        trigger: descs,
+        start: "top 85%",
+        once: true
+      }
     });
 
-    // auto-cleanup on context revert
-    return () => split.revert();
-
   }, scope);
-
-  return ctx;
 }
